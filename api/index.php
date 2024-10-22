b515194628e89321e6427811c883527c0d7ba448
Lucas Wagner de Melo Fogaça <141058064+LucasWMF@users.noreply.github.com>
2024-10-22 17:47:10 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -12,7 +12,6 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/root.css">
     <link rel="icon" href="/img/logo.png">
-    <link rel="stylesheet" href="../public/css/root.css">
 </head>
 
 <body>
